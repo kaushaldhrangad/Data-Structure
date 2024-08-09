45adb5348cd1fe1977e311563fda9beb105a4ba7
Kaushal Dhrangad <kaushaldhrangad4@gmail.com>
2024-08-09 18:21:21 +0530

    q:- Rotate an Array by K places

--- Array.cpp
+++ Array.cpp
@@ -162,3 +162,28 @@ vector<int> rotateArray(vector<int> &arr, int n) {
   arr[n - 1] = temp;
   return arr;
 }
+
+
+// Rotate an Array by K places
+/*
+Example:
+'arr '= [1,2,3,4,5]
+'k' = 1  rotated array = [2,3,4,5,1]
+'k' = 2  rotated array = [3,4,5,1,2]
+'k' = 3  rotated array = [4,5,1,2,3] and so on.
+*/
+
+vector<int> rotateArray(vector<int> arr, int k) {
+  // Write your code here.
+  int n = arr.size();
+  for (int i = 0; i < k; i++) {
+
+    int temp = arr[0];
+    for (int j = 0; j < n - 1; j++) {
+      arr[j] = arr[j + 1];
+      // j++;
+    }
+    arr[n - 1] = temp;
+  }
+  return arr;
+}
