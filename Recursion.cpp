a74806763ce165a3c55e7f5576b0969af8cdf371
Kaushal Dhrangad <kaushaldhrangad4@gmail.com>
2024-08-11 18:39:42 +0530

    q:- sum of n numbers | sum of n terms | factorial using recursion

--- Recursion.cpp
+++ Recursion.cpp
@@ -39,3 +39,74 @@ class Solution {
     }
 };
 
+// Sum of first N numbers
+/*
+Input: n = 3
+Output: 6
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int sum(int n) {
+	// your code goes here
+	
+	if(n==0) return 0;
+	return n + sum(n-1);
+
+}
+
+int main() {
+	// your code goes here
+	int n;
+	cin >> n;
+	cout << sum(n);
+    return 0;
+}
+
+
+
+// Factorial Using Recursion
+/*  
+Input: n = 5
+Output: 120
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int fact(int n) {
+	// your code goes here
+	
+	if(n==0) return 1; //Return 1 because after the condition met this is add into function if we write 0 then answer multiply with function and answer will be 0.
+	return n * fact(n-1);
+}
+
+int main() {
+	// your code goes here
+	int n;
+	cin >> n;
+	cout << fact(n);
+    return 0;
+}
+
+// Sum of first n terms
+/*
+Input:
+n=5
+
+Output:
+225
+
+Explanation:
+(1*1*1) + (2*2*2) + (3*3*3) + (4*4*4) + (5*5*5) = 225
+*/
+
+class Solution {
+  public:
+    long long sumOfSeries(long long n) {
+        // code here
+        
+        if(n==0) return 0;
+        return (n*n*n) + sumOfSeries(n-1);
+    }
+};
